eb62a8b45806ad49ae5938e6451b13b77ab7eebb
Julien D. <julien.ducharme.z@gmail.com>
2025-10-16 21:50:47 -0400

    Edited config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,29 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
+// https://astro.build/config for more information
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+    // basic site config
+    site: "https://wiki.zauraac.xyz",
+    trailingSlash: "ignore",
+    
+    integrations: [
+	starlight({
+	    title: 'My Docs',
+	    social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+	    sidebar: [
+		{
+		    label: 'Guides',
+		    items: [
+			// Each item here is one entry in the navigation menu.
+			{ label: 'Example Guide', slug: 'guides/example' },
+		    ],
+		},
+		{
+		    label: 'Reference',
+		    autogenerate: { directory: 'reference' },
+		},
+	    ],
+	}),
+    ],
 });
